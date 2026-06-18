8aa036af0325906ec0668ae14f187c250938d981
Qays Alnajjad <103290698+QaysAlnajjad@users.noreply.github.com>
2026-06-18 11:23:20 +0400

    Update terraform-verify.sh

--- scripts/deployment-automation-scripts/terraform-verify.sh
+++ scripts/deployment-automation-scripts/terraform-verify.sh
@@ -3,200 +3,51 @@
 set -e
 
 
-source "$(dirname "$0")/config.sh"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-source "$(dirname "$0")/stacks_config.sh"
+
+source "$SCRIPT_DIR/config.sh"
+source "$SCRIPT_DIR/stacks_config.sh"
+
+
+echo "DEBUG:"
+echo "TF_STATE_BUCKET_NAME=$TF_STATE_BUCKET_NAME"
+echo "TF_STATE_BUCKET_REGION=$TF_STATE_BUCKET_REGION"
 
 
 
 verify_stack(){
 
-
 stack=$1
 
 
 echo "======================"
-
 echo "Checking $stack"
-
 echo "======================"
 
 
-
 terraform -chdir="environments/$stack" init \
-
 -reconfigure \
-
 -backend-config="bucket=$TF_STATE_BUCKET_NAME" \
-
 -backend-config="key=environments/$stack/terraform.tfstate" \
-
 -backend-config="region=$TF_STATE_BUCKET_REGION"
-
-
 
 
 terraform -chdir="environments/$stack" fmt -check
 
 
-
 terraform -chdir="environments/$stack" validate
 
 
-
-
-echo "Checking remote state..."
-
-
-
-STATE_EXISTS=$(aws s3api head-object \
-
---bucket "$TF_STATE_BUCKET_NAME" \
-
---key "environments/$stack/terraform.tfstate" \
-
-2>/dev/null || true)
-
-
-
-
-if [ -z "$STATE_EXISTS" ]; then
-
-
-echo "⚠️ No remote state found for $stack"
-
-echo "Running first deployment plan"
-
+echo "Running terraform plan"
 
 
 terraform -chdir="environments/$stack" plan \
-
 ${STACK_VARS[$stack]} \
-
 -no-color
-
-
-
-RESULT=$?
-
-
-
-else
-
-
-echo "Remote state found"
-
-echo "Running drift detection plan"
-
-
-
-set +e
-
-
-terraform -chdir="environments/$stack" plan \
-
-${STACK_VARS[$stack]} \
-
--no-color \
-
--detailed-exitcode
-
-
-
-RESULT=$?
-
-
-
-set -e
-
-
-
-fi
-
-
-
-
-
-case $RESULT in
-
-
-
-0)
-
-echo "✅ $stack OK"
-
-;;
-
-
-
-2)
-
-echo "⚠️ $stack has changes"
-
-;;
-
-
-
-1)
-
-echo "❌ $stack terraform error"
-
-exit 1
-
-;;
-
-
-
-*)
-
-echo "❌ Unexpected terraform exit code $RESULT"
-
-exit $RESULT
-
-;;
-
-
-
-esac
-
-
 
 }
 
 
 
-
 verify_stack "global/iam"
-
-verify_stack "global/oac"
-
-
-verify_stack "primary/network"
-
-verify_stack "primary/rds"
-
-
-verify_stack "dr/network"
-
-
-verify_stack "primary/alb"
-
-
-verify_stack "primary/ecs"
-
-verify_stack "dr/ecs"
-
-
-verify_stack "operations/dr_orchestration"
-
-
-verify_stack "primary/failover_alarms"
-
-
-
-
-echo "======================"
-
-echo "Verification completed"
-
-echo "======================"
