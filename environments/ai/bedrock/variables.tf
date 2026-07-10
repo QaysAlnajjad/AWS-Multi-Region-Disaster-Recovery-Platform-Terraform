f4cd82c824eeac9b0399f63e60a15b3faa335242
Qays Alnajjad <103290698+QaysAlnajjad@users.noreply.github.com>
2026-07-10 11:00:33 +0400

    Update variables.tf

--- environments/ai/bedrock/variables.tf
+++ environments/ai/bedrock/variables.tf
@@ -14,6 +14,6 @@ variable "embedding_model_arn" {
 
   type = string
 
-  default = "wordpress-ai-knowledge"
+  default = "arn:aws:bedrock:us-east-1::foundation-model/amazon.titan-embed-text-v2:0"
 
 }
