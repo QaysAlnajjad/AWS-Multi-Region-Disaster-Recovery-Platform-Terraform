a86b5acefa9a3c427f1ca899b2a709e2305784e5
Qays Alnajjad <103290698+QaysAlnajjad@users.noreply.github.com>
2026-07-10 13:50:13 +0400

    Update main.tf

--- environments/ai/foundation/main.tf
+++ environments/ai/foundation/main.tf
@@ -193,13 +193,5 @@ resource "aws_opensearchserverless_collection" "vector" {
 
   type = "VECTORSEARCH"
 
-  depends_on = [
-
-    aws_opensearchserverless_security_policy.encryption,
-
-    aws_opensearchserverless_security_policy.network
-
-  ]
-
 }
 
