6a6c84cb1ce381d624f05a682a98f0abf6377692
Qays Alnajjad <103290698+QaysAlnajjad@users.noreply.github.com>
2026-07-10 23:16:03 +0400

    Update main.tf

--- environments/ai/bedrock/main.tf
+++ environments/ai/bedrock/main.tf
@@ -229,7 +229,7 @@ resource "aws_opensearchserverless_access_policy" "access" {
       Principal = [
 
         module.bedrock_role.role_arn,
-        arn:aws:iam::127214183643:role/github-actions-terraform-role
+        "arn:aws:iam::127214183643:role/github-actions-terraform-role"
 
       ]
 
