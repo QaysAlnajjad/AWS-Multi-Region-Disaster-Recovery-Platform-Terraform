f9591f4958bf0503f4fd496d26d722c4cdcbd533
Qays Alnajjad <103290698+QaysAlnajjad@users.noreply.github.com>
2026-07-10 17:44:58 +0400

    Update outputs.tf

--- environments/ai/foundation/outputs.tf
+++ environments/ai/foundation/outputs.tf
@@ -6,6 +6,10 @@ output "bucket_arn" {
   value = module.knowledge_bucket.bucket_arn
 }
 
+output "bucket_name" {
+  value = module.knowledge_bucket.bucket_name
+}
+
 output "collection_arn" { 
   value = aws_opensearchserverless_collection.vector.arn
 }
