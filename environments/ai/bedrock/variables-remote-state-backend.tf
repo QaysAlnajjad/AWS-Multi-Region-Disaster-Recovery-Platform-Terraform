61d1fc6e5348e1a3e09494ecc1c6b626c6373ea5
Qays Alnajjad <103290698+QaysAlnajjad@users.noreply.github.com>
2026-07-10 19:29:25 +0400

    Update variables-remote-state-backend.tf

--- environments/ai/bedrock/variables-remote-state-backend.tf
+++ environments/ai/bedrock/variables-remote-state-backend.tf
@@ -1,9 +1,11 @@
 variable "state_bucket_name" {
   description = "S3 bucket name for Terraform state"
   type        = string
+  default = "wordpress-ai-bedrock-135498"
 }
 
 variable "state_bucket_region" {
   description = "S3 bucket region for Terraform state"
   type        = string
+  default = "eu-central-1"
 }
